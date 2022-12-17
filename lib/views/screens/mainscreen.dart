a49756a6b6138accccdc42621c2d03de531428ac
LimKitEeNatelie <limkitee@gmail.com>
2022-12-17 13:59:31 +0800

    first commit

---
+++ lib/views/screens/mainscreen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../../models/user.dart';
+import '../shared/mainmenu.dart';
+
+class MainScreen extends StatefulWidget {
+  final User user;
+  const MainScreen({super.key, required this.user});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+
+  @override
+   Widget build(BuildContext context) {
+    return WillPopScope(
+        onWillPop: () async => false,
+        child: Scaffold(
+          appBar: AppBar(title: const Text("HomeStay")),
+          body: const Center(child: Text("HomeStay")),
+          drawer:  MainMenuWidget(user:widget.user),
+        ));
+  }
+}
---
+++ lib/views/screens/mybookingsscreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import '../../models/user.dart';
+
+class MyBookingsScreen extends StatefulWidget {
+  const MyBookingsScreen({super.key,});
+  @override
+  State<MyBookingsScreen> createState() => _MyBookingsState();
+}
+
+class _MyBookingsState extends State<MyBookingsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
